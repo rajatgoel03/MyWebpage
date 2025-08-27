e6af367599787dd589d6ec19203ded41fd2a4e78
Rajat Goyal <65103100+rajatgoel03@users.noreply.github.com>
2025-08-27 16:50:01 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        maroon: {
+          '50': '#fdf2f2',
+          '100': '#fbe7e7',
+          '200': '#f8dədə',
+          '300': '#f2caca',
+          '400': '#e9abab',
+          '500': '#dd8787',
+          '600': '#c96262',
+          '700': '#a84747',
+          '800': '#8a3d3d',
+          '900': '#703535',
+          '950': '#4b1d1d',
+        },
+      },
       fontFamily: {
         // This sets up 'Inter' as a custom font family, matching the original design.
         sans: ['Inter', 'sans-serif'],
